e34679902fe9e86d4119dd385e262cfc78b5d58b
caio-cunha <caiocomputacao2014@gmail.com>
2022-05-20 20:32:16 -0300

    fix: fix app of date chart

--- brmed/cotacao/exceptions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.exceptions import APIException
-
-class DataNotFound(APIException):
-    status_code = 404
-    default_detail = {
-        'code': 1422, 'msg': 'Não há dados no banco de dados'}
-    default_code = 6003   
